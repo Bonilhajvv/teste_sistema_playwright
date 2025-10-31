ddeb3254256b6621793fcbf7bb1ff28b8b2e9dc0
bonilhajvv <joaovictorbonilhaventurinib@gmail.com>
2025-10-31 19:35:05 -0300

    feat: implementar caso de teste de event

--- tests/event.spec.ts
+++ tests/event.spec.ts
@@ -5,6 +5,7 @@ import { LoginPage } from '../pages/login/login.page';
 import { AddEventPage } from '../pages/event/add-event.list.page';
 
 
+
 test.describe('Gestão de Tipos de Eventos (HU 5.1)', () => {
     let loginPage: LoginPage;
     let dashboardPage: DashboardPage;
@@ -27,30 +28,28 @@ test.describe('Gestão de Tipos de Eventos (HU 5.1)', () => {
     });
 
     test('Cenário 1: Deve cadastrar um novo tipo de evento com sucesso', async ({ page }) => {
-        const eventName = `Evento de Teste ${Date.now()}`;
-        const eventDescription = 'Esta é uma descrição de teste para o evento.';
-        const data = {
-            eventName,
-            eventDescription
-        }
-        await addEventPage.fillEventForm(data); 
+        const eventData = {
+            eventName: `Evento de Teste ${Date.now()}`,
+            eventDescription: 'Esta é uma descrição de teste para o evento.'
+        };
+
+        await addEventPage.fillEventForm(eventData);
 
         await eventListPage.assertIsOnPage();
 
-        await expect(page.getByText(eventName)).toBeVisible();
+        await expect(page.getByText(eventData.eventName)).toBeVisible();
     });
 
     test('Cenário 2: Não deve cadastrar sem preencher campos obrigatórios', async ({ page }) => {
-                const eventName = `Evento de Teste ${Date.now()}`;
-        const eventDescription = null;
-        const data = {
-            eventName,
-            eventDescription
-        }
-        await addEventPage.fillEventForm(data); 
+
+        const eventDataScenario = {
+            eventName: `Evento de Teste ${Date.now()}`,
+            eventDescription: null
+        };
+        await addEventPage.fillEventForm(eventDataScenario);
 
         await eventListPage.assertIsOnPage();
 
-        await expect(page.getByText(eventName)).toBeVisible();
+        await expect(page.getByText(eventDataScenario.eventName)).toBeVisible();
     });
 });
